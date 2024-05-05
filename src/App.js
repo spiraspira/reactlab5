6fd4f2c70f8e87be74401e38def9ea026ae7e34e
spiraspira <zabornik5350@gmail.com>
2024-05-06 02:48:29 +0300

    Add Footer

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRouter from "./router/AppRouter";
 import './App.css';
+import Footer from "./components/Footer";
 
 function App() {
   return (
-           <BrowserRouter>
-               <AppRouter/>
-            </BrowserRouter>
+    <div className="app">
+        <BrowserRouter>
+           <AppRouter/>
+        </BrowserRouter>
+        <Footer/>
+    </div>
+           
 );
 };
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+  class Footer extends Component {
+    render() {
+      return <footer>© 2024 Агентство недвижимости</footer>;
+    }
+  }
+
+export default Footer;
