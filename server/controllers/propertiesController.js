83eebdb82d1bc095be1afc93b69b1aebd5adb390
spiraspira <zabornik5350@gmail.com>
2024-05-10 03:22:42 +0300

    Add CRUD ops for PropertiesController

--- server/controllers/propertiesController.js
+++ server/controllers/propertiesController.js
@@ -1,29 +1,116 @@
 const fs = require('fs');
 const path = require('path');
 
-// Функция для чтения файла с данными Properties
-function readProperties(callback) {
+// GET controller for retrieving property data
+const getProperties = (req, res) => {
+  const filePath = path.join(__dirname, '..', 'data', 'properties.json');
+
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Server error' });
+    }
+
+    const properties = JSON.parse(data);
+    res.json(properties);
+  });
+};
+
+// POST controller for creating a new property
+const createProperty = (req, res) => {
+  const newProperty = req.body;
+
   const filePath = path.join(__dirname, '..', 'data', 'properties.json');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error(err);
-      return callback(err, null);
+      return res.status(500).json({ error: 'Server error' });
     }
-    const properties = JSON.parse(data);
-    callback(null, properties);
-  });
-}
 
-// Обработчик GET-запроса для получения данных Properties
-function getProperties(req, res) {
-  readProperties((err, properties) => {
-    if (err) {
-      return res.status(500).json({ error: 'Ошибка сервера' });
-    }
-    res.json(properties);
+    const properties = JSON.parse(data);
+
+    const newPropertyId = generateUniqueId();
+    newProperty.id = newPropertyId;
+
+    properties.push(newProperty);
+
+    fs.writeFile(filePath, JSON.stringify(properties), 'utf8', err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Server error' });
+      }
+
+      res.json({ success: true, id: newPropertyId });
+    });
   });
-}
+};
+
+// PUT controller for updating property data
+const updateProperty = (req, res) => {
+  const propertyId = req.params.id;
+  const updatedProperty = req.body;
+
+  const filePath = path.join(__dirname, '..', 'data', 'properties.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Server error' });
+    }
+
+    const properties = JSON.parse(data);
+
+    const property = properties.find(p => p.id === propertyId);
+    if (!property) {
+      return res.status(404).json({ error: 'Property not found' });
+    }
+
+    Object.assign(property, updatedProperty);
+
+    fs.writeFile(filePath, JSON.stringify(properties), 'utf8', err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Server error' });
+      }
+
+      res.json({ success: true });
+    });
+  });
+};
+
+// DELETE controller for deleting a property
+const deleteProperty = (req, res) => {
+  const propertyId = req.params.id;
+
+  const filePath = path.join(__dirname, '..', 'data', 'properties.json');
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'Server error' });
+    }
+
+    const properties = JSON.parse(data);
+
+    const propertyIndex = properties.findIndex(p => p.id === propertyId);
+    if (propertyIndex === -1) {
+      return res.status(404).json({ error: 'Property not found' });
+    }
+
+    properties.splice(propertyIndex, 1);
+
+    fs.writeFile(filePath, JSON.stringify(properties), 'utf8', err => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Server error' });
+      }
+
+      res.json({ success: true });
+    });
+  });
+};
 
 module.exports = {
-  getProperties
+  getProperties,
+  createProperty,
+  updateProperty,
+  deleteProperty
 };
--- server/routes/properties.js
+++ server/routes/properties.js
@@ -1,25 +1,19 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 
 const router = express.Router();
 
+const propertiesController = require('../controllers/propertiesController');
+
 // GET-маршрут для получения данных Properties
-router.get('/', (req, res) => {
-  // Путь к файлу с данными Properties
-  const filePath = path.join(__dirname, '..', 'data', 'properties.json');
+router.get('/', propertiesController.getProperties);
 
-  // Чтение файла с данными Properties
-  fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'Ошибка сервера' });
-    }
+// POST-маршрут для создания нового сообщения
+router.post('/', propertiesController.createProperty);
 
-    // Преобразование данных в формат JSON и отправка клиенту
-    const properties = JSON.parse(data);
-    res.json(properties);
-  });
-});
+// PUT-маршрут для обновления данных сообщения
+router.put('/:id', propertiesController.updateProperty);
+
+// DELETE-маршрут для удаления сообщения
+router.delete('/:id', propertiesController.deleteProperty);
 
 module.exports = router;
