1f0a42a0ecb85faca8426b3848754ee51755dbba
spiraspira <zabornik5350@gmail.com>
2024-05-15 00:41:13 +0300

    Add validation

--- server/models/models.js
+++ server/models/models.js
@@ -2,31 +2,88 @@ const sequelize = require('.');
 const { DataTypes } = require('sequelize');
 
 const Property = sequelize.define('Property', {
-    Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING, allowNull: true },
-    description: { type: DataTypes.STRING, allowNull: true }
+  Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
 },
-{timestamps: false});;
+{ timestamps: false });
 
 const Message = sequelize.define('Message', {
-    Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING, allowNull: true },
-    email: { type: DataTypes.STRING, allowNull: true },
-    message: { type: DataTypes.STRING, allowNull: true },
-    date: { type: DataTypes.DATE, allowNull: true }
+  Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isEmail: true,
+    },
+  },
+  message: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isDate: true,
+    },
+  },
 },
-{timestamps: false});;
+{ timestamps: false });
 
 const Testimonial = sequelize.define('Testimonial', {
-    Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    name: { type: DataTypes.STRING, allowNull: true },
-    testimonial: { type: DataTypes.STRING, allowNull: true },
-    date: { type: DataTypes.DATE, allowNull: true }
+  Id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  testimonial: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  date: {
+    type: DataTypes.DATE,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+      isDate: true,
+    },
+  },
 },
-{timestamps: false});;
+{ timestamps: false });
 
 module.exports = {
-    Property,
-    Message,
-    Testimonial
+  Property,
+  Message,
+  Testimonial
 };
